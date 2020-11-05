2bcd64105c444586e07ccf5abdd7183780555c2e
ianhudson87 <ihudson@wustl.edu>
2020-11-04 22:44:28 -0600

    changed styling

--- my-project/pages/UserProfile.js
+++ my-project/pages/UserProfile.js
@@ -114,7 +114,7 @@ const styles = StyleSheet.create({
     //flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    fontSize: "30px",
+    fontSize: 30,
     fontWeight: "bold",
   },
   usersContainer: {
