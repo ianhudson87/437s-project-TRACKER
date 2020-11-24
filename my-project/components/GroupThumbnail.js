782bd27ff1b5236754c8fd3fe333659b2dc3283b
Callan Logan <c.logan@wustl.edu>
2020-11-24 13:57:20 -0700

    Changed group thumbnail

--- my-project/components/GroupThumbnail.js
+++ my-project/components/GroupThumbnail.js
@@ -42,7 +42,7 @@ render() {
         <Card.Title>{this.state.name} <Icon size={20} name="chevron-right" onPress={this.goToGroup} /></Card.Title>
         <Card.Divider/>
         <Text style={{marginBottom: 10}}>
-            The idea with React Native Elements is more about component structure than actual design.
+            Description for {this.state.name}
         </Text>
     </Card>
   )
